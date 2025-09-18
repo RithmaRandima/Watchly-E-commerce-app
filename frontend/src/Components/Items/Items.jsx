f23ff48c0873608bfdc42d5210c48488b83c4f4c
RithmaRandima <rithma.r.nandasiri@gmail.com>
2025-09-18 10:18:37 +0530

    completed Item component

--- frontend/src/Components/Items/Items.jsx
+++ frontend/src/Components/Items/Items.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Items.css";
 
-const Items = () => {
-  return <div>Items</div>;
+const Items = (props) => {
+  return (
+    <div className="item">
+      <img src={props.image} alt="" />
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">{props.item_price_new}</div>
+        <div className="item-price-old">{props.item_price_old}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Items;
